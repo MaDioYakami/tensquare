18ff44f12bbebc543e041abd5297ea30179ac21c
madio <694140163@qq.com>
2020-11-22 04:01:13 +0800

    十次方项目-添加吐槽模块，主要是springCloud中对Eureka和Feign的支持调用，理解了用Feign调用不同服务之间的流程

---
+++ tensquare/tensquare_qa/src/main/java/com/tensquare/qa/client/BaseClient.java
@@ -0,0 +1,16 @@
+package com.tensquare.qa.client;
+
+import com.tensquare.qa.client.impl.BaseClientImpl;
+import entity.Result;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+//@FeignClient注解用于指定从哪个服务中调用功能 ，注意 里面的名称与被调用的服务 名保持一致，并且不能包含下划线。
+@FeignClient(value = "tensquare-base", fallback = BaseClientImpl.class)
+public interface BaseClient {
+    /*@RequestMapping注解用于对被调用的微服务进行地址映射。注意 @PathVariable注解一定要指定参数名称，否则出错*/
+    @RequestMapping(value = "/label/{labelId}", method = RequestMethod.GET)
+    public Result findById(@PathVariable("labelId") String labelId);
+}
